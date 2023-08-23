c2775b87657bb72bc00b9e8db21f7bee8f95ab51
brettdavidbowles <brett.bowles@gmail.com>
2023-08-23 17:28:16 -0400

    commented out static root for migration

--- sls_baseball/settings.py
+++ sls_baseball/settings.py
@@ -175,9 +175,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if env('ENVIRONMENT') == 'production':
-    STATIC_ROOT = env('STATIC_ROOT')
-    STATICFILES_DIRS = [BASE_DIR / "static"]
+# if env('ENVIRONMENT') == 'production':
+#     STATIC_ROOT = env('STATIC_ROOT')
+#     STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
