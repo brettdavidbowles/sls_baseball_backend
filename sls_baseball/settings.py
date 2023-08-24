6ee8a3317ab83941379790f9c98ee71d37cc3a5a
brettdavidbowles <brett.bowles@gmail.com>
2023-08-24 17:07:34 -0400

    staticfiles_dirs

--- sls_baseball/settings.py
+++ sls_baseball/settings.py
@@ -178,6 +178,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # if env('ENVIRONMENT') == 'production':
 #     STATIC_ROOT = env('STATIC_ROOT')
