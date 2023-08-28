01755890592ba65ee71deed90ca569b9c903cba8
brettdavidbowles <brett.bowles@gmail.com>
2023-08-28 17:00:34 -0400

    moved staticfiles dir

--- sls_baseball/settings.py
+++ sls_baseball/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "checkstatic/static/")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 # if env('ENVIRONMENT') == 'production':
 #     STATIC_ROOT = env('STATIC_ROOT')
 #     STATICFILES_DIRS = [BASE_DIR / "static"]
