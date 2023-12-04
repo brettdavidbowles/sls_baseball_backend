73d1e5ae02e6f32ec50465319c41e578b534aabf
brettdavidbowles <brett.bowles@gmail.com>
2023-12-04 18:00:13 -0500

    nevermind, being impatient

--- sls_baseball/settings.py
+++ sls_baseball/settings.py
@@ -35,13 +35,12 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 ALLOWED_HOSTS = ['baseballsimulator.online',
                  'slsbaseballbackend-production.up.railway.app',
-                 'https://sls-baseball-frontend.vercel.app',
-                 #  'localhost'
+                 'https://sls-baseball-frontend.vercel.app'
                  ]
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_SAMESITE = 'None'
-CSRF_COOKIE_SAMESITE = 'None'
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SAMESITE = 'None'
+# CSRF_COOKIE_SAMESITE = 'None'
 # CSRF_COOKIE_DOMAIN = 'https://sls-baseball-frontend.vercel.app'
 CSRF_TRUSTED_ORIGINS = ['https://baseballsimulator.online',
                         'https://www.baseballsimulator.online',
