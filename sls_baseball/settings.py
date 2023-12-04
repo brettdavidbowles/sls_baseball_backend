b5e35e09f9b9994a0a9ea6423b3911e2d0dfa83a
brettdavidbowles <brett.bowles@gmail.com>
2023-12-04 18:23:29 -0500

    cors_origin_allow_all

--- sls_baseball/settings.py
+++ sls_baseball/settings.py
@@ -49,7 +49,7 @@ CSRF_TRUSTED_ORIGINS = ['https://baseballsimulator.online',
                         ]
 # CSRF_COOKIE_DOMAIN = 'https://slsbaseballbackend-production.up.railway.app'
 # SESSION_COOKIE_SECURE = True
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     #     "https://baseballsimulator.online",
